ba3d6cee7862c560f5a4fd108d6d48241a309b13
illbmbst <alex.krivushenko@gmail.com>
2019-04-17 18:33:10 +0300

    week 4 initial commit

---
+++ week4/src/Solver.java
@@ -0,0 +1,22 @@
+public class Solver {
+
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvable() {
+
+    }
+
+    public int moves() {
+
+    }
+
+    public Iterable<Board> solution() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
