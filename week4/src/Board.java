7e61f40dc631e7707efe86aecdd1bc0306c7ca50
illbmbst <alex.krivushenko@gmail.com>
2019-05-26 13:32:55 +0300

    hamming method

--- week4/src/Board.java
+++ week4/src/Board.java
@@ -1,42 +1,65 @@
 public class Board {
 
-    public Board(int[][] blocks) {
+    private int[][] blocks;
 
+    public Board(int[][] blocks) {
+        this.blocks = blocks;
     }
 
     public int dimension() {
-
+        return blocks.length;
     }
 
     public int hamming() {
-
+        int hamming = 0;
+        int count = 1;
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                int block = blocks[i][j];
+                if (block != 0 && block != count) {
+                    hamming++;
+                }
+                count++;
+            }
+        }
+        return hamming;
     }
 
     public int manhattan() {
-
+        return 0;
     }
 
     public boolean isGoal() {
-
+        return false;
     }
 
     public Board twin() {
-
+        return null;
     }
 
     public boolean equals(Object y) {
-
+        return false;
     }
 
     public Iterable<Board> neighbors() {
-
+        return null;
     }
 
     public String toString() {
-
+        return null;
     }
 
     public static void main(String[] args) {
+        testHamming();
+    }
 
+    private static void testHamming() {
+        Board board1 = new Board(new int[][]{
+                new int[]{1, 2, 3},
+                new int[]{4, 5, 6},
+                new int[]{7, 0, 8}
+        });
+        int result1 = board1.hamming();
+        System.out.println(String.format("Board1: expected = %d, result = %d", 1, result1));
     }
 }
