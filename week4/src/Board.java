3a5535b042ed29e707252049beba85d5d8acfd2d
illbmbst <alex.krivushenko@gmail.com>
2019-05-28 09:47:12 +0300

    twin, equals methods implemented

--- week4/src/Board.java
+++ week4/src/Board.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Board {
 
     private int[][] blocks;
@@ -13,8 +15,8 @@ public class Board {
     public int hamming() {
         int hamming = 0;
         int count = 1;
-        for (int i = 0; i < blocks.length; i++) {
-            for (int j = 0; j < blocks[i].length; j++) {
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
                 int block = blocks[i][j];
                 if (block != 0 && block != count) {
                     hamming++;
@@ -30,15 +32,42 @@ public class Board {
     }
 
     public boolean isGoal() {
-        return false;
+        return hamming() == 0;
     }
 
     public Board twin() {
-        return null;
+        int aX, aY;
+        do {
+            aX = StdRandom.uniform(1, 10);
+            aY = StdRandom.uniform(1, 10);
+        } while (blocks[aX][aY] != 0);
+        int bX, bY;
+        do {
+            bX = StdRandom.uniform(1, 10);
+            bY = StdRandom.uniform(1, 10);
+        } while (blocks[bX][bY] != 0);
+
+        Board newBoard = copy();
+        int a = newBoard.blocks[aX][aY];
+        int b = newBoard.blocks[bX][bY];
+        newBoard.blocks[aX][aY] = b;
+        newBoard.blocks[bX][bY] = a;
+        return newBoard;
     }
 
     public boolean equals(Object y) {
-        return false;
+        Board that = (Board) y;
+        if (this.dimension() != that.dimension()) {
+            return false;
+        }
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                if (this.blocks[i][j] != that.blocks[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public Iterable<Board> neighbors() {
@@ -49,8 +78,20 @@ public class Board {
         return null;
     }
 
+    private Board copy() {
+        int[][] result = new int[dimension()][dimension()];
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                result[i][j] = blocks[i][j];
+            }
+        }
+        return new Board(result);
+    }
+
     public static void main(String[] args) {
         testHamming();
+        testManhattan();
+        testEquals();
     }
 
     private static void testHamming() {
@@ -61,5 +102,30 @@ public class Board {
         });
         int result1 = board1.hamming();
         System.out.println(String.format("Board1: expected = %d, result = %d", 1, result1));
+    }
+
+    private static void testManhattan() {
+        Board board1 = new Board(new int[][]{
+                new int[]{8, 2, 3},
+                new int[]{4, 0, 6},
+                new int[]{7, 5, 1}
+        });
+        int result1 = board1.manhattan();
+        System.out.println(String.format("Board1: expected = %d, result = %d", 8, result1));
+    }
+
+    private static void testEquals() {
+        Board board1 = new Board(new int[][]{
+                new int[]{8, 2, 3},
+                new int[]{4, 0, 6},
+                new int[]{7, 5, 1}
+        });
+        Board board2 = new Board(new int[][]{
+                new int[]{8, 2, 3},
+                new int[]{4, 0, 6},
+                new int[]{7, 5, 1}
+        });
+        boolean result1 = board1.equals(board2);
+        System.out.println(String.format("Expected = %b, result = %b", true, result1));
     }
 }
